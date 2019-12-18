2b23427f0dd10c57e6421a9cec0a7fa837819fe7
Ryan Hoffmann <ryan.hoffmann@airwis.com>
2019-12-18 16:22:40 -0600

    i like this blue better

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles(theme => ({
     position: 'relative'
   },
   generated: {
-    color: blue[400],
+    color: blue[900],
   },
   typography: {
     marginBottom: theme.spacing(1),
